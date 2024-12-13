5c199b0e03729ff72343ba0e02860e01c46b9d9a
dey-desing <dhaiayr4@gmail.com>
2024-12-12 22:42:07 -0500

    git commit -m "Actualización: validación de entradas en el juego Piedra, Papel o Tijeras"

--- PiedraPapelTijeras.py
+++ PiedraPapelTijeras.py
@@ -13,11 +13,17 @@ puntaje_sistema = 0
 
 while jugar_de_nuevo:
     #Entrada del jugador
-    jugador = input("Elige piedra, papel o tijeras: ")
-    print(f"Tú elegiste: {jugador}")
+    jugador = input("Elige piedra, papel o tijeras: ").lower()
+    print(f"Tú elegiste: {jugador}")    
+    #Validacion entrada jugador
+    if jugador not in opciones:
+        print("Opción no válida. Intenta de nuevo.")
+        continue
+    
     #Elección del sistema
     sistema= random.choice(opciones)
     print(f"El sistema eligió: {sistema}")
+    
     #Reglas del juego
     if jugador == sistema:
         print("Es un empate.")
@@ -29,11 +35,18 @@ while jugar_de_nuevo:
     else:
         print("Perdiste.")
         puntaje_sistema += 1
+        
     #Puntajes
     print (f"Puntaje: {puntaje_jugador} Jugador - {puntaje_sistema} Sistema")
-    #Jugar de nuevo
-    respuesta = input("Quieres jugar otra vez? (si/no):")
-    if respuesta != "si":
-        jugar_de_nuevo = False #Variable booleana
+    
+    #Validar si quiere jugar de nuevo
+    respuesta = input("¿Quieres jugar otra vez? (si/no): ").lower()
+    while respuesta not in ["si", "no"]:
+        print("Opción no válida. Por favor, responde con 'si' o 'no'")
+        respuesta = input("¿Quieres jugar otra vez? (si/no): ").lower()
+    
+    if respuesta == "no":
+        jugar_de_nuevo = False
+
 #Terminar juego       
 print ("Gracias por jugar!")
